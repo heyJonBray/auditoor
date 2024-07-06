4f27fd7a1da3611a93b74310394487f54410cfb7
Jon Bray <jon@heyjonbray.com>
2024-07-06 02:36:54 -0400

    fix: simplify splash screen image

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -4,6 +4,9 @@ import { Button } from 'frames.js/next';
 const logoUrl =
   'https://aquamarine-evil-bedbug-307.mypinata.cloud/ipfs/QmVr4zKLB96MUKuRYCAAjjLhQrn6c3k1fUVh7LVkynbSk5';
 
+const backgroundImageUrl =
+  'https://aquamarine-evil-bedbug-307.mypinata.cloud/ipfs/QmYUb8YoQp5EPGYCrZjKFppepsAP5DENn6qHa97DnH8Ytg';
+
 const handler = frames(async () => {
   return {
     image: (
@@ -15,98 +18,14 @@ const handler = frames(async () => {
           justifyContent: 'space-between',
           height: '100vh',
           width: '100vw',
-          background: '#2e2e2e',
           color: 'white',
           padding: '20px',
+          backgroundImage: `url(${backgroundImageUrl})`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-            padding: '20px',
-          }}
-        >
-          <h1 style={{ fontSize: '35px', color: 'white', marginTop: '0' }}>
-            The Growth Cult
-          </h1>
-          <div style={{ padding: '10px 0' }}></div>
-          <h1
-            style={{
-              fontSize: '75px',
-              color: 'lightblue',
-              margin: 0,
-              padding: 0,
-            }}
-          >
-            Anti
-          </h1>
-          <h1
-            style={{
-              fontSize: '75px',
-              color: 'lightblue',
-              margin: '-30px 0 0 0',
-              padding: 0,
-            }}
-          >
-            Rug
-          </h1>
-          <h1
-            style={{
-              fontSize: '75px',
-              color: 'lightblue',
-              margin: '-30px 0 10px 0',
-              padding: 0,
-            }}
-          >
-            System
-          </h1>
-          <p
-            style={{ fontSize: '25px', color: 'white', margin: 0, padding: 0 }}
-          >
-            he inspecc
-          </p>
-          <p
-            style={{
-              fontSize: '25px',
-              color: 'white',
-              margin: '-5px 0',
-              padding: 0,
-            }}
-          >
-            he protecc
-          </p>
-          <p
-            style={{
-              fontSize: '25px',
-              color: 'white',
-              margin: '-5px 0',
-              padding: 0,
-            }}
-          >
-            but mostly, he help u not get rekt
-          </p>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-            width: '50%',
-          }}
-        >
-          <img
-            src={logoUrl}
-            alt="Logo"
-            style={{
-              width: '250px',
-              height: '250px',
-            }}
-          />
-        </div>
         <p
           style={{
             position: 'absolute',
