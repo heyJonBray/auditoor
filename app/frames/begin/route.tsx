2435be01ba00ac4684f33f53c1122232dd842c34
Jon Bray <jon@heyjonbray.com>
2024-07-06 22:17:27 -0400

    feat: add NFT gated splash screen

--- app/frames/begin/route.tsx
+++ app/frames/begin/route.tsx
@@ -6,74 +6,169 @@ import {
   TokenBlockchain,
 } from '@airstack/frames';
 
+// testing addresses, comment out the one that isn't needed
+// test address = true
+// const nftAddress = '0xec5461aa3a8cac1095b04d00ac7cabab87a2a7ec';
+// test address = false
+const nftAddress = '0xec5461aa3a8cac1095b04d00ac7cabab87a2a7ee';
+
 const handler = frames(
   async (ctx) => {
     console.log(ctx.isAllowed); // check if user is allowed to access frame
-    return {
-      // todo: alt image if no NFT
-      image: (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-            width: '100vw',
-            background: '#2e2e2e',
-            color: 'white',
-            textAlign: 'center',
-            padding: '20px',
-          }}
-        >
-          <h1 style={{ fontSize: '60px', marginTop: '30px' }}>
-            Contract Auditoor Frame
-          </h1>
-          <h1 style={{ fontSize: '40px', marginTop: '10px' }}>
-            Run a security check on tokens before you buy!
-          </h1>
-          <p
+
+    if (ctx.isAllowed) {
+      return {
+        image: (
+          <div
             style={{
-              position: 'absolute',
-              bottom: '10px',
-              right: '25px',
-              fontWeight: 'bold',
-              fontSize: '25px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh',
+              width: '100vw',
+              background: '#2e2e2e',
+              color: 'white',
+              textAlign: 'center',
+              padding: '20px',
+              position: 'relative',
             }}
           >
-            Powered by QuickIntel API
-          </p>
-        </div>
-      ),
-      buttons: [
-        <Button
-          action="post"
-          target={{ pathname: '/chain1', query: { foo: 'bar' } }}
-        >
-          Start
-        </Button>,
-        <Button action="link" target={'https://warpcast.com/jonbray.eth'}>
-          FAQ
-        </Button>,
-      ],
-    };
+            <h1
+              style={{
+                fontSize: '40px',
+                position: 'absolute',
+                top: '20px',
+                left: '20px',
+                marginTop: 20,
+                marginLeft: 20,
+              }}
+            >
+              The Growth Cult
+            </h1>
+            <h1
+              style={{
+                fontSize: '75px',
+                marginTop: '30px',
+                color: 'lightblue',
+              }}
+            >
+              Anti Rug System
+            </h1>
+            <h1 style={{ fontSize: '40px', marginTop: '10px' }}>
+              Run a security check on tokens before you buy!
+            </h1>
+            <p
+              style={{
+                position: 'absolute',
+                bottom: '10px',
+                right: '25px',
+                fontWeight: 'bold',
+                fontSize: '25px',
+              }}
+            >
+              plz do not rely solely on outputs from the ARS, scammers gonna
+              scam!
+            </p>
+          </div>
+        ),
+        buttons: [
+          <Button
+            action="post"
+            target={{ pathname: '/chain1', query: { foo: 'bar' } }}
+          >
+            Start
+          </Button>,
+        ],
+      };
+    } else {
+      return {
+        image: (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh',
+              width: '100vw',
+              background: '#2e2e2e',
+              color: 'white',
+              textAlign: 'center',
+              padding: '20px',
+              position: 'relative',
+            }}
+          >
+            <h1
+              style={{
+                fontSize: '40px',
+                position: 'absolute',
+                top: '20px',
+                left: '20px',
+                marginTop: 20,
+                marginLeft: 20,
+              }}
+            >
+              The Growth Cult
+            </h1>
+            <h1
+              style={{
+                fontSize: '75px',
+                marginTop: '30px',
+                color: 'lightblue',
+              }}
+            >
+              Anti Rug System
+            </h1>
+            <h1 style={{ fontSize: '40px', marginTop: '10px' }}>
+              This service is only available to ARS Pass holders. Please
+              purchase a pass to continue.
+            </h1>
+            <p
+              style={{
+                position: 'absolute',
+                right: '25px',
+                top: '85%',
+                transform: 'translateX(48%)',
+                textAlign: 'center',
+                width: '50%',
+                fontSize: 50,
+              }}
+            >
+              ⬇️
+            </p>
+          </div>
+        ),
+        buttons: [
+          <Button action="post" target={{ pathname: '/begin' }}>
+            Refresh
+          </Button>,
+          <Button
+            action="link"
+            target={'https://example.com/subscribe'} // Replace with NFT mint URL
+          >
+            Purchase
+          </Button>,
+        ],
+      };
+    }
   },
   {
     middleware: [
       allowList({
-        apiKey: process.env.NEXT_PUBLIC_AIRSTACK_API_KEY as string,
+        apiKey: process.env.AIRSTACK_API_KEY as string,
         criteria: {
           or: [
             [
               AllowListCriteria.TOKEN_HOLD,
               {
                 chain: TokenBlockchain.Base,
-                tokenAddress: '0xec5461aa3a8cac1095b04d00ac7cabab87a2a7ec', // paid group stub (test NFT)
+                tokenAddress: nftAddress,
               },
             ],
           ],
         },
-      }) as unknown as any, // force compatible type
+      }) as unknown as any,
     ],
   }
 );
--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -2,10 +2,7 @@ import { frames } from './frames';
 import { Button } from 'frames.js/next';
 
 const logoUrl =
-  'https://aquamarine-evil-bedbug-307.mypinata.cloud/ipfs/QmVr4zKLB96MUKuRYCAAjjLhQrn6c3k1fUVh7LVkynbSk5';
-
-const backgroundImageUrl =
-  'https://aquamarine-evil-bedbug-307.mypinata.cloud/ipfs/QmYUb8YoQp5EPGYCrZjKFppepsAP5DENn6qHa97DnH8Ytg';
+  'https://aquamarine-evil-bedbug-307.mypinata.cloud/ipfs/QmdJc9iofMdwvym8Fg3kKDu3V5REdVu5FszYFdm6DWLeg4';
 
 const handler = frames(async () => {
   return {
@@ -18,14 +15,98 @@ const handler = frames(async () => {
           justifyContent: 'space-between',
           height: '100vh',
           width: '100vw',
+          background: '#2e2e2e',
           color: 'white',
           padding: '20px',
-          // backgroundImage: `url(${backgroundImageUrl})`,
-          // backgroundSize: 'contain',
-          // backgroundPosition: 'center',
-          // backgroundRepeat: 'no-repeat',
         }}
       >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
+            padding: '20px',
+          }}
+        >
+          <h1 style={{ fontSize: '35px', color: 'white', marginTop: '0' }}>
+            The Growth Cult
+          </h1>
+          <div style={{ padding: '5px 0' }}></div>
+          <h1
+            style={{
+              fontSize: '75px',
+              color: 'lightblue',
+              margin: '-20px 0 0 0',
+              padding: 0,
+            }}
+          >
+            Anti
+          </h1>
+          <h1
+            style={{
+              fontSize: '75px',
+              color: 'lightblue',
+              margin: '-30px 0 0 0',
+              padding: 0,
+            }}
+          >
+            Rug
+          </h1>
+          <h1
+            style={{
+              fontSize: '75px',
+              color: 'lightblue',
+              margin: '-30px 0 10px 0',
+              padding: 0,
+            }}
+          >
+            System
+          </h1>
+          <p
+            style={{ fontSize: '30px', color: 'white', margin: 0, padding: 0 }}
+          >
+            he inspecc
+          </p>
+          <p
+            style={{
+              fontSize: '30px',
+              color: 'white',
+              margin: '-5px 0',
+              padding: 0,
+            }}
+          >
+            he protecc
+          </p>
+          <p
+            style={{
+              fontSize: '30px',
+              color: 'white',
+              margin: '-5px 0',
+              padding: 0,
+            }}
+          >
+            but mostly, he help u not get rekt
+          </p>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+            width: '50%',
+          }}
+        >
+          <img
+            src={logoUrl}
+            alt="Logo"
+            style={{
+              width: '250px',
+              height: '250px',
+            }}
+          />
+        </div>
         <p
           style={{
             position: 'absolute',
@@ -45,9 +126,6 @@ const handler = frames(async () => {
         target={{ pathname: '/begin', query: { foo: 'bar' } }}
       >
         Start
-      </Button>,
-      <Button action="link" target={'https://warpcast.com/jonbray.eth'}>
-        Mint
       </Button>,
       <Button action="link" target={'https://warpcast.com/jonbray.eth'}>
         FAQ
